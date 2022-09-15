b9ad930608f477cdea8d4d2ee6733e0ed6331428
RRH-INF2 <41006574+RRH-INF2@users.noreply.github.com>
2022-09-15 15:07:39 +0200

    Create NouvelleStar.java

---
+++ NouvelleStar.java
@@ -0,0 +1,35 @@
+
+import java.util.Random;
+
+public class NouvelleStar {
+
+   public static void main(String[] args) {
+      final int ARGS_LENGTH = args.length;
+      if (ARGS_LENGTH == 0) {
+         System.out.println("Il n’existe pas de candidats !");
+      } else {
+         // Affichage des candidats
+         System.out.println("Candidats:");
+         for (int i = 0; i < ARGS_LENGTH; ++i) {
+            System.out.println("#" + (i + 1) + " " + args[i]);
+         }
+         // Votes
+         final int NB_VOTES = 150;
+         System.out.println("\n" + NB_VOTES + " votes:");
+         int[] votes = new int[ARGS_LENGTH];
+         Random r = new Random();
+         for (int i = 0; i < NB_VOTES; ++i) {
+            System.out.print('.');
+            votes[r.nextInt(ARGS_LENGTH)]++; // nbre entre 0 et ARGS_LENGTH - 1
+         }
+         // Affichage des résultats
+         System.out.println("\n\nRésultats:");
+         long pourcentage;
+         final double COEFF = 100. / NB_VOTES;
+         for (int i = 0; i < ARGS_LENGTH; ++i) {
+            pourcentage = Math.round(COEFF * votes[i]);
+            System.out.println(pourcentage + "% " + args[i]);
+         }
+      }
+   }
+}
